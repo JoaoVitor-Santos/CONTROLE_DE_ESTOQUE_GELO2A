5c95ae5f608a91397a877d152c41b246c537f9f7
Joao Vitor Santos <joaovitorsgdev@gmail.com>
2025-02-05 19:38:26 -0300

    Primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyle from './styles/global';
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      Hello World
+    </div>
+  );
+}
+
+export default App;
